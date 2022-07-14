fefd65fa19b8f689653f24760a7924611eb42723
wilsonibekason <wanbekgithub@gmail.com>
2022-07-14 16:27:00 +0100

    complete create js-concept class

---
+++ index.js
@@ -0,0 +1,20 @@
+var wilson = 'wiklson';
+console.log(wilson);
+//   creating for numbers
+var myAge = 24;
+console.log(myAge);
+//   creating for binary numbers
+var isPublished = 2;
+console.log(isPublished);
+//   creating for undefined
+var getStatus = undefined;
+console.log(getStatus);
+var ss2ClassBoys = ["Uche","Jeremy","Fortune","Campbell","Somto","Kachi"]
+console.log(ss2ClassBoys);
+//var getIndividualName = ss2ClassBoys[0];
+//   creating for function
+function DisplayName(names){
+    let getIndividualName = names[0];
+}
+let displayName = DisplayName(ss2ClassBoys);
+console.log("The name of the individual is" + ' ' + displayName)
